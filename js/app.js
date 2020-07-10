e7015b5dff3b420b91b9d4d8186f3c6a26a33960
collinskoech11 <collinskoechck34@gmail.com>
2020-07-10 14:53:41 +0300

    video scroll animation complete

--- js/app.js
+++ js/app.js
@@ -7,12 +7,30 @@ const end = section.querySelector('h1');
 
 //scroll magic
 const controller = new ScrollMagic.Controller();
-
+//scenes
 const scene = new ScrollMagic.Scene({
-	duration:2000,
+	duration:9000,
 	triggerElemnt: intro,
 	triggerHook: 0
 })
  .addIndicators()
  .setPin(intro)
  .addTo(controller);
+//video animation
+
+let accelamount = 0.5;
+let scrollpos = 0;
+let delay = 0;
+
+
+scene.on("update", e =>{
+	scrollpos = e.scrollPos / 1000;
+	
+});
+
+setInterval(() => {
+	delay += (scrollpos - delay)*accelamount;
+	console.log(scrollpos, delay);
+
+	video.currentTime = scrollpos
+}, 33.3);
